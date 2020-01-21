952613fa436b282c46ebc50b918f56e139ccd9f5
Aleksandr Maramzin <maramzinalexandr@gmail.com>
2020-01-21 14:31:42 +0000

    [!] Fixed Graph class

--- include/Graph.h
+++ include/Graph.h
@@ -1,55 +1,68 @@
 #ifndef ABSTRACT_GRAPH_H
 #define ABSTRACT_GRAPH_H
 
+#include <vector>
+#include <map>
+#include <iostream>
+
 namespace abstract {
-
-template <typename NodeType>
-class GraphNode {
-
-    public:
-        
-        GraphNode(NodeType node_data)
-            : data(node_data) {}
-
-        ~GraphNode() {}
-
-        NodeType get_node_data() { return data; }
-
-    private:
-        NodeType data;
-};
-
-template <typename EdgeType>
-class GraphEdge {
-
-    public:
-        
-        GraphEdge(EdgeType edge_data)
-            : data(edge_data) {}
-
-        ~GraphNode() {}
-
-        EdgeType get_edge_data() { return data; }
-
-    private:
-        EdgeType data;
-};
 
 template <typename NodeType, typename EdgeType> 
 class Graph {
 
     public:    
+
+        using node_iterator = typename std::vector<NodeType>::iterator;
+        using edge_iterator = typename std::map<std::pair<int,int>,EdgeType>::iterator;
         
-        Graph() {}
+
+        Graph(int size) { nodes.reserve(size); }
         ~Graph() {}
 
-        add_edge(NodeType from, NodeType to, EdgeType edge);
+        node_iterator node_begin() { return nodes.begin(); }
+        node_iterator node_end() { return nodes.end(); }
+ 
+        edge_iterator edge_begin() { return edges.begin(); }
+        edge_iterator edge_end() { return edges.end(); }
+        
+        std::size_t node_size() const { return nodes.size(); }
+        std::size_t edge_size() const { return edges.size(); }
+        
+        void add_node(int node_index, NodeType node) {
+            nodes[node_index] = node;
+        }
+
+        void add_edge(std::pair<int,int> from_to_index_pair, EdgeType edge) {
+            edges[from_to_index_pair] = edge;
+        }
+
+        EdgeType& find_edge(const std::pair<int,int>& edge) {
+            auto edge_it = edges.find(edge);
+            if (edge_it != edges.end()) {
+                return edge_it->second;
+            } else {
+                return invalid_edge;
+            }
+        }
+
+        void print_nodes(std::ostream& outs) {
+            for (auto it = nodes.begin(); it != nodes.end(); it++) {
+                outs << "node[" << it->first << "] = { " << it->second << " }" << std::endl;
+            }
+        }
+        
+    public:
+
+        static EdgeType invalid_edge;
 
     private:
-        
-        std::unordered_set<GraphNode<NodeType>> nodes;
-        std::unordered_map<std::pair<GraphNode<NodeType>,GraphNode<NodeType>>,> nodes;
+
+        std::vector<NodeType> nodes; // graph nodes are numbered as vector indecies
+        std::map<std::pair<int,int>,EdgeType> edges;
 };
+
+template <typename NodeType,typename EdgeType>
+EdgeType Graph<NodeType,EdgeType>::invalid_edge = EdgeType();
 
 } // namespace abstract
 
