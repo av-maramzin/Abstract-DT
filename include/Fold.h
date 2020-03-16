901616d2a421b75bb86254298cd4513a3fc6951d
Aleksandr Maramzin <maramzinalexandr@gmail.com>
2020-03-16 15:19:31 +0000

    [!] Fixed Fold template implementation

--- include/Fold.h
+++ include/Fold.h
@@ -3,59 +3,58 @@
 
 namespace abstract {
 
-template <typename ElemType, typename ComputeType>
+template <typename ElemType, typename ComputeType, typename SeedType>
 class Fold {
 
     public:
         
         class Element {
-            
-            virtual ComputeType compute(ComputeType) = 0;
+            public:
+                Element() {}
+                ~Element() {}
+
+                virtual SeedType inject(SeedType seed) = 0;
+                virtual ComputeType compute(ComputeType) = 0;
 
             private:
+                SeedType 
                 int depth;
         };
 
         Fold();
-        
-        template <typename SeedType>
+        ~Fold();
+       
+        void inject(SeedType seed);
         void grow(int depth);
-        
-        template <typename ComputeType>
-        void compute(ComputeType);
+        ComputeType compute();
 
     private:
-
         std::vector<std::unique_ptr<Element>> elements;
-
 };
 
-template <typename ElemType>
-template <typename SeedType>
-void Fold<ElemType>::grow(int depth) {
-
+template <typename ElemType, typename ComputeType, typename SeedType>
+void Fold<ElemType,ComputeType,SeedType>::grow(int depth) {
     for (size_t i = depth; i > 0; i--) {
-        
         std::unique_ptr<Element> elem(new ElemType());
-        
         elements.push_back(elem);
     }
-
-
 }
 
-template <typename ElemType>
-void Fold<ElemType>::compute(int depth) {
+template <typename ElemType, typename ComputeType, typename SeedType>
+void Fold<ElemType,ComputeType,SeedType>::inject(SeedType seed) {
+    SeedType ret = elements[0]->inject(seed);
+    for (size_t i = 1; i < depth; i++) {
+        ret = elements[i]->inject(ret);
+    }
+}
 
-    ComputeType ret = elements[depth]->compute();
+template <typename ElemType, typename ComputeType, typename SeedType>
+void Fold<ElemType,ComputeType,SeedType>::compute(int depth) {
+    ComputeType ret;
+    ret = elements[depth]->compute(ret);
     for (size_t i = depth-1; i > 0; i--) {
         ret = elements[i]->compute(ret);
     }
-
-
 }
-
-Fold<struct Lateral>::Element
-
 
 #endif // #ifndef ABSTRACT_FRACTAL_H
