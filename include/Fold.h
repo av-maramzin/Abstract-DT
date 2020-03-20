f1f63c3422fa1e2a1e388b7b3ea1c766a8a779a0
Aleksandr <maramzinalexandr@gmail.com>
2020-03-20 18:06:40 +0000

    [+] Next version of the Fold class

--- include/Fold.h
+++ include/Fold.h
@@ -3,59 +3,74 @@
 
 namespace abstract {
 
-template <typename ElemType, typename ComputeType>
+template <typename ElemType, typename ComputeType, typename SeedType>
 class Fold {
 
     public:
-        
+
+        using Compute_t = ComputeType;
+        using Element_t = ElemType;
+
+        // [*] Usage:
+        // 
+        // Users of the Fold class template must specify their own 
+        // type of elements to be folded by deriving it from below
+        // Element class. The latter serves as the backbone of the 
+        // fold, knows its depth and provides an interface function
+        // compute() to be overriden by concrete classes of elements
+        //
         class Element {
             
-            virtual ComputeType compute(ComputeType) = 0;
+            public:
+
+                Element() : depth(-1) {}
+
+                virtual ComputeType compute(ComputeType) = 0;
+                virtual SeedType inject(SeedType) = 0;
 
             private:
                 int depth;
         };
 
         Fold();
-        
-        template <typename SeedType>
+        ~Fold();
+       
+        // [*] Global interface to the Fold class
         void grow(int depth);
-        
-        template <typename ComputeType>
-        void compute(ComputeType);
+        void inject();
+        ComputeType compute();
 
     private:
-
+        int depth;
         std::vector<std::unique_ptr<Element>> elements;
-
 };
 
-template <typename ElemType>
-template <typename SeedType>
-void Fold<ElemType>::grow(int depth) {
-
+template <typename ElemType, typename ComputeType, typename SeedType>
+void Fold<ElemType,ComputeType,SeedType>::grow(int depth) {
     for (size_t i = depth; i > 0; i--) {
-        
         std::unique_ptr<Element> elem(new ElemType());
-        
         elements.push_back(elem);
     }
-
-
 }
 
-template <typename ElemType>
-void Fold<ElemType>::compute(int depth) {
-
-    ComputeType ret = elements[depth]->compute();
+template <typename ElemType, typename ComputeType, typename SeedType>
+void Fold<ElemType,ComputeType,SeedType>::compute() {
+    ComputeType ret;
+    ret = elements[depth]->compute(ret);
     for (size_t i = depth-1; i > 0; i--) {
         ret = elements[i]->compute(ret);
     }
+}
 
+template <typename ElemType, typename ComputeType, typename SeedType>
+SeedType Fold<ElemType,ComputeType,SeedType>::inject(SeedType seed) {
+    SeedType s;
+    s = elements[0]->inject(seed);
+    for (size_t i = 1; i < depth; i++) {
+        s = elements[i]->inject(s);
+    }
+}
 
 }
 
-Fold<struct Lateral>::Element
-
-
-#endif // #ifndef ABSTRACT_FRACTAL_H
+#endif // #ifndef ABSTRACT_FOLD_H
